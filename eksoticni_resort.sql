58c4af702d819fa97064e3193b8e229aab59d30c
majbc1999 <majbc1999@gmail.com>
2021-08-20 13:00:35 +0200

    sql

--- eksoticni_resort.sql
+++ eksoticni_resort.sql
@@ -1,5 +1,5 @@
 DROP TABLE IF EXISTS gost;
-DROP TABLE IF EXISTS soba;
+DROP TABLE IF EXISTS sobe;
 DROP TABLE IF EXISTS zaposleni;
 DROP TABLE IF EXISTS nastanitve;
 DROP TABLE IF EXISTS hrana;
@@ -16,9 +16,9 @@ CREATE TABLE gost (
 );
 
 
-CREATE TABLE soba (
+CREATE TABLE sobe (
     stevilka    INTEGER     PRIMARY KEY,
-    cena        INTEGER         NOT NULL,
+    cena        INTEGER     NOT NULL,
     postelje    INTEGER     NOT NULL
 );
 
@@ -26,18 +26,18 @@ CREATE TABLE soba (
 CREATE TABLE zaposleni (
     emso        CHAR    PRIMARY KEY,
     ime         CHAR    NOT NULL,
-    priimek     CHAR    NOT NULL,    
+    priimek     CHAR    NOT NULL,
+    spol        CHAR    NOT NULL,    
     placa       INTEGER NOT NULL,
     oddelek     CHAR    NOT NULL
 );
 
 
 CREATE TABLE nastanitve (
-    id              AUTO_INCREMENT   PRIMARY KEY,            
-    gost            CHAR            REFERENCES gost(emso),
-    datum_prihoda   DATE            NOT NULL,
-    datum_odhoda    DATE            NOT NULL,
-    soba            INTEGER         REFERENCES soba(stevilka)     NOT NULL
+    id              INTEGER         UNIQUE                 PRIMARY KEY,            
+    gost_id         CHAR            REFERENCES gost(emso)          NOT NULL,
+    datum           DATE            NOT NULL,
+    soba_id            INTEGER         REFERENCES sobe(stevilka)   NOT NULL
 );
 
 
@@ -54,6 +54,6 @@ CREATE TABLE ciscenje (
     id          AUTO_INCREMENT   PRIMARY KEY,
     cistilka    CHAR            REFERENCES zaposleni(emso),
     datum       DATE            NOT NULL,
-    soba        INTEGER         REFERENCES soba(stevilka)
+    soba_id        INTEGER         REFERENCES sobe(stevilka)
 );
 
---
+++ podatki/nastanitve.sql
@@ -0,0 +1,2 @@
+insert into nastanitve (gost_id, datum, soba_id) values ('8d750abe-170c-4d39-bc1d-40f0e4518a41', '2021-08-19', 13);
+insert into nastanitve (gost_id, datum, soba_id) values ('59afab46-7ea0-4790-98ed-d8b5f949b13e', '2021-08-16', 13);
